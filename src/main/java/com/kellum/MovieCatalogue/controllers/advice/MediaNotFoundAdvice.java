db5c339ec3177c84241050c14c8534f8b690d847
Chris <christopherk379@gmail.com>
2022-11-04 02:23:44 -0400

    adding media not found advice

---
+++ src/main/java/com/kellum/MovieCatalogue/controllers/advice/MediaNotFoundAdvice.java
@@ -0,0 +1,19 @@
+package com.kellum.MovieCatalogue.controllers.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.kellum.MovieCatalogue.exceptions.MediaNotFoundException;
+
+@ControllerAdvice
+public class MediaNotFoundAdvice {
+    @ResponseBody
+    @ExceptionHandler(MediaNotFoundException.class)    
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String mediaNotFoundHandler(MediaNotFoundException mex) {
+        return mex.getMessage();
+    }
+}
