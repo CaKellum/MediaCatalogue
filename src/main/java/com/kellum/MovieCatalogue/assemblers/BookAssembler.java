329b21397113017826c6b9751ca2ea667fa248cc
Chris <christopherk379@gmail.com>
2022-11-09 19:18:03 -0500

    add implementation of book assembler

--- src/main/java/com/kellum/MovieCatalogue/assemblers/BookAssembler.java
+++ src/main/java/com/kellum/MovieCatalogue/assemblers/BookAssembler.java
@@ -1,18 +1,22 @@
 package com.kellum.MovieCatalogue.assemblers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import com.kellum.MovieCatalogue.controllers.BookController;
 import com.kellum.MovieCatalogue.model.Book;
 
 @Component
 public class BookAssembler implements RepresentationModelAssembler<Book, EntityModel<Book>> {
 
     @Override
-    public EntityModel<Book> toModel(Book entity) {
-        // TODO Auto-generated method stub
-        return null;
+    public EntityModel<Book> toModel(Book book) {
+        return EntityModel.of(book, 
+                linkTo(methodOn(BookController.class).getById(book.getId())).withSelfRel(),
+                linkTo(methodOn(BookController.class).all()).withRel("books"));
     }
 
 }
