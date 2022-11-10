8374138ec73b9dcc4d0a2226cc4c2f732fa62ce6
Chris <christopherk379@gmail.com>
2022-11-09 19:20:41 -0500

    Add implementation of MovieAsembler

--- src/main/java/com/kellum/MovieCatalogue/assemblers/MovieAssembler.java
+++ src/main/java/com/kellum/MovieCatalogue/assemblers/MovieAssembler.java
@@ -1,16 +1,19 @@
 package com.kellum.MovieCatalogue.assemblers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 
+import com.kellum.MovieCatalogue.controllers.MovieController;
 import com.kellum.MovieCatalogue.model.Movie;
 
 public class MovieAssembler implements RepresentationModelAssembler<Movie, EntityModel<Movie>> {
 
     @Override
-    public EntityModel<Movie> toModel(Movie entity) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    public EntityModel<Movie> toModel(Movie movie) {
+        return EntityModel.of(movie, //
+        linkTo(methodOn(MovieController.class).getById(movie.getId())).withSelfRel(),
+        linkTo(methodOn(MovieController.class).all()).withRel("movie"));
 
 }
