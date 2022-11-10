13ee63ae8756e202c30eca351cefa20e3ca4d774
Chris <christopherk379@gmail.com>
2022-11-09 19:35:37 -0500

    Adds TVshowAssembler implementation

--- src/main/java/com/kellum/MovieCatalogue/assemblers/TVShowAssembler.java
+++ src/main/java/com/kellum/MovieCatalogue/assemblers/TVShowAssembler.java
@@ -1,16 +1,19 @@
 package com.kellum.MovieCatalogue.assemblers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 
+import com.kellum.MovieCatalogue.controllers.TVShowController;
 import com.kellum.MovieCatalogue.model.TvShow;
 
 public class TVShowAssembler implements RepresentationModelAssembler<TvShow, EntityModel<TvShow>> {
 
     @Override
-    public EntityModel<TvShow> toModel(TvShow entity) {
-        // TODO Auto-generated method stub
-        return null;
+    public EntityModel<TvShow> toModel(TvShow tvShow) {
+        return EntityModel.of(tvShow, //
+                linkTo(methodOn(TVShowController.class).getById(tvShow.getId())).withSelfRel(),
+                linkTo(methodOn(TVShowController.class).all()).withRel("tvShow"));
     }
 
 }
