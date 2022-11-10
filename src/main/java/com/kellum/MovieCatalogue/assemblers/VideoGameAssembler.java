8595067e2a69bd07077171949b34c14699fbee8f
Chris <christopherk379@gmail.com>
2022-11-09 19:36:07 -0500

    Add VideogameAssembler Implementation

--- src/main/java/com/kellum/MovieCatalogue/assemblers/VideoGameAssembler.java
+++ src/main/java/com/kellum/MovieCatalogue/assemblers/VideoGameAssembler.java
@@ -1,16 +1,19 @@
 package com.kellum.MovieCatalogue.assemblers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 
+import com.kellum.MovieCatalogue.controllers.VideoGameController;
 import com.kellum.MovieCatalogue.model.VideoGame;
 
 public class VideoGameAssembler implements RepresentationModelAssembler<VideoGame, EntityModel<VideoGame>> {
 
     @Override
-    public EntityModel<VideoGame> toModel(VideoGame entity) {
-        // TODO Auto-generated method stub
-        return null;
+    public EntityModel<VideoGame> toModel(VideoGame videoGame) {
+        return EntityModel.of(videoGame, //
+                linkTo(methodOn(VideoGameController.class).getById(videoGame.getId())).withSelfRel(),
+                linkTo(methodOn(VideoGameController.class).all()).withRel("VideoGame"));
     }
-    
+
 }
