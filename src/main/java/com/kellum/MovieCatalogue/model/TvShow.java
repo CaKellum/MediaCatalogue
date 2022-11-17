b0f98232e333434d62ac42796399f21dd5e06b91
Chris <christopherk379@gmail.com>
2022-11-16 23:59:59 -0500

    added extra fields to tvshow

--- src/main/java/com/kellum/MovieCatalogue/model/TvShow.java
+++ src/main/java/com/kellum/MovieCatalogue/model/TvShow.java
@@ -4,10 +4,22 @@ import javax.persistence.Entity;
 import org.hibernate.annotations.Polymorphism;
 import org.hibernate.annotations.PolymorphismType;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Polymorphism(type=PolymorphismType.EXPLICIT)
 public class TvShow extends Media {
-    public TvShow(String title, MediaFormat format) {
+
+    @Getter
+    @Setter
+    private int seasons;
+    
+    @Getter
+    @Setter
+    private int episodes;
+
+    public TvShow(String title, MediaFormat format, int seasons, int episodes) {
         super(title, MediaCategory.TV_SHOW, format);
     }
 }
